affa111cbedacb80178dc40a623b07c6eefce92c
FelipeBCouto <59423627+FelipeBCouto@users.noreply.github.com>
2023-04-21 22:17:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ app.listen(4000, () => {
     console.log("Aplicação rodando na porta 4000")
 })
 
-// req (request) = recebe dados os dados da requisição.
-// res (response) = retorna os dados requisitados na rota. Algumas possíveis respostas são:
-//      a) response.json({message:”retorno de objeto json”}) = retorna um objeto json para a página
-//      b) response.send(“ ”) = envia algo para a página como um texto (é o caso abaixo)
-// '/' = rota inicial, acessada pelo link http://localhost:4000/
 app.get('/', (req, res) =>{
     res.send("Opa, beleza? :D")
 })
