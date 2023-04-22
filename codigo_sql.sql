7db1f6e63dd53315e4a469fffd228852043a362d
FelipeBCouto <59423627+FelipeBCouto@users.noreply.github.com>
2023-04-21 22:14:26 -0300

    Delete codigo_sql.sql

--- codigo_sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE nome_do_BD; -- escolha o nome do BD
-
-USE nome_do_BD;
-
-CREATE TABLE tasks(
-	id int auto_increment not null primary key,
-    tarefa varchar(85) not null,
-    descricao text
-);
