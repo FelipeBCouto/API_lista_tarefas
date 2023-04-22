0ff9d8e9c650571a97686f00298fa92465c7fac2
FelipeBCouto <59423627+FelipeBCouto@users.noreply.github.com>
2023-04-21 21:13:11 -0300

    Update TaskController.js

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -40,30 +40,12 @@ class TaskController{
         const {tarefa} = req.body
         const {descricao} = req.body
         
-        database.where({id:id}).update({tarefa:tarefa}).table("tasks").then(data=>{
+        database.where({id:id}).update({tarefa:tarefa}).update({descricao:descricao}).table("tasks").then(data=>{
             res.json({message:"Tarefa atualizada com sucesso"})
         }).catch(error=>{
             res.json(error)
         })
     }
-
-    /*
-    ALTERE CASO DESEJE ATUALIZAR A TAREFA E DESCRIÇÃO
-    atualizarTarefa(req, res){
-        const { id } = req.params
-        const {tarefa} = req.body
-        const {descricao} = req.body
-        
-        database.where({id:id}).update({tarefa:tarefa}).table("tasks").catch(error=>{
-            res.json(error)
-        }),
-        database.where({id:id}).update({descricao:descricao}).table("tasks").then(data=>{
-            res.json({message:"Tarefa atualizada com sucesso"})
-        }).catch(error=>{
-            res.json(error)
-        })
-    }
-    */
 
     removerTarefa(req, res){
         const { id } = req.params
